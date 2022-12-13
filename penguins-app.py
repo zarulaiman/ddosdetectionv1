a6f86c907781a7e962a81373b343e7a390fcfb87
Muhammad Hazree <84779758+pucau@users.noreply.github.com>
2022-12-13 11:37:30 +0800

    fix_load_error

--- penguins-app.py
+++ penguins-app.py
@@ -17,66 +17,36 @@ st.sidebar.markdown("""
 
 # Collects user input features into dataframe
 uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
-#if uploaded_file is not None:
-input_df = pd.read_csv(uploaded_file)
-#else:
-    #def user_input_features():
-        #island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
-        #sex = st.sidebar.selectbox('Sex',('male','female'))
-        #bill_length_mm = st.sidebar.slider('Bill length (mm)', 0,99999)
-        #bill_depth_mm = st.sidebar.slider('Bill depth (mm)', 13.1,21.5,17.2)
-        #flipper_length_mm = st.sidebar.slider('Flipper length (mm)', 172.0,231.0,201.0)
-        #body_mass_g = st.sidebar.slider('Body mass (g)', 2700.0,6300.0,4207.0)
-        #data = {'island': island,
-        #        'bill_length_mm': bill_length_mm,
-        #        'bill_depth_mm': bill_depth_mm,
-        #        'flipper_length_mm': flipper_length_mm,
-        #        'body_mass_g': body_mass_g,
-        #        'sex': sex}
-        #features = pd.DataFrame(data, index=[0])
-        #return features
-#input_df = df.reset_index()
-df = input_df
+if uploaded_file is not None:
+    input_df = pd.read_csv(uploaded_file)
+    df = input_df
+else:
+    st.sidebar.write("Before upload, text ni akan appear, lepas upload csv text ni akan hilang, pandai2 kau atur")
 
-# Combines user input features with entire penguins dataset
-# This will be useful for the encoding phase
-#penguins_raw = pd.read_csv('DDoSselectedfeatures.csv')
-#penguins = penguins_raw.drop(columns=['Label'], axis=1)
-#df = pd.concat([input_df,penguins],axis=0)
-
-# Encoding of ordinal features
-# https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
-#encode = ['sex','island']
-#for col in encode:
-    #dummy = pd.get_dummies(df[col], prefix=col)
-    #df = pd.concat([df,dummy], axis=1)
-    #del df[col]
-#df = df[:1] # Selects only the first row (the user input data)
 
 # Displays the user input features
 st.subheader('User Input features')
 
-#just incase
+# test if else menjadi
 if uploaded_file is not None:
     st.write(df)
+    # Reads in saved classification model
+    load_clf = pickle.load(open('DDOS_RFC.pkl', 'rb'))
+
+    # Apply model to make predictions
+    prediction = load_clf.predict(df)
+    prediction_proba = load_clf.predict_proba(df)
+
+    st.subheader('Prediction')
+    penguins_species = np.array(['BENIGN', 'DDoS'])
+
+    st.write(penguins_species[prediction])
+
+    st.subheader('Prediction Probability')
+    st.write(prediction_proba)
+
+
 else:
-    st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
-    st.write(df)
+    st.write('# --Awaiting Czarul to upload CSV File--')
+    st.write('--mcm biasa text ni hilang lepas kau upload file--')
 
-# Reads in saved classification model
-load_clf = pickle.load(open('DDOS_RFC.pkl', 'rb'))
-
-# Apply model to make predictions
-prediction = load_clf.predict(df)
-prediction_proba = load_clf.predict_proba(df)
-
-
-st.subheader('Prediction')
-penguins_species = np.array(['BENIGN','DDoS'])
-#test
-
-#test
-st.write(penguins_species[prediction])
-
-st.subheader('Prediction Probability')
-st.write(prediction_proba)
