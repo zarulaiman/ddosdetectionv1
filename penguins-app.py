a69e49c213d4e150b24d8e089e564f65bafef1bb
zarulaiman <106679748+zarulaiman@users.noreply.github.com>
2023-01-15 01:44:55 +0800

    Update penguins-app.py

--- penguins-app.py
+++ penguins-app.py
@@ -9,28 +9,35 @@ import streamlit.components.v1 as components
 #test
 st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
 st.markdown("""
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #FFA500;">
-  <a class="navbar-brand" href="http://localhost:8501/" target="_blank">Machine Learning system using Random Forest Classifier</a>
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark" style="background-color: #1a1a1a; color: white; text-shadow: 2px 2px 5px black;">
+  <a class="navbar-brand" >Machine Learning system using Random Forest Classifier</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 </nav>
 """, unsafe_allow_html=True)
 
+st.markdown(
+    """
+<style>
+.sidebar .sidebar-content {
+    background-image: linear-gradient(#2e7bcf,#001433);
+    color: white;
+}
+</style>
+""",
+    unsafe_allow_html=True,
+)
+
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
-
-st.header('_Machine Learning system using Random Forest Classifier_')
-#st.header('A header with _italics_ :blue[colors]: and emojis :sunglasses:')
-
 st.write("""
 # DDoS Attack Detection System (Random Forest Classifier)
 """)
 
 st.sidebar.header('User Input Features')
 
-st.sidebar.markdown("""
+st.sidebar.write("""
 [Example CSV input file](https://raw.githubusercontent.com/zarulaiman/ddosdetectionv1/main/penguins_example.csv?token=GHSAT0AAAAAAB5GCAUKNSIIUIVJXBCDJQDAY5Z2VYQ)
 """)
 
@@ -71,3 +78,13 @@ if uploaded_file is not None:
 
 else:
     st.write('# --Awaiting User to upload CSV File--')
+    st.write('# !ATTENTION!')
+    st.write('HOW TO USE THIS WEBSITE? ')
+    st.write('- Firstly, make sure that the .csv file have these features (The order does not matter):-')
+    st.image('list.png')
+    st.write('- Second, make sure the input data do not have any null, infinite or more than 3.4E+38 value or this error will popup')
+    st.image('error1.png')
+    st.write('- Third, on the left of the website there will be a section to upload the input files.')
+    st.write('- Lastly, wait for the application to scan your input files and detect which one is the DDoS Attack or Benign.')
+    st.write('  The system will show the input that were uploaded followed by the prediction for every row of the input and end with the probability that the prediction.')
+    st.image('result.png')
